c26aa45f11d269202d7e135c0c68d58e53e7db5d
santiago <ttsbpn@gmail.com>
2020-06-25 22:14:51 -0500

    updated index.js

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 
-app.get('/', (res, req) => {
+app.get('/holi', (res, req) => {
     res.json({
         text: 'api works!'
     });
 });
 
+app.post('/login', (req, res) => {
+    const user = {id: 3};
+    const token = jwt.sign({user},'myToken');
+    res.json({
+        token
+    });
+});
 
+app.get('/api/protected', (res, req) => {
+    res.json({
+        text: 'protected'
+    })
+});
 
 app.listen(3000, () =>{
     console.log('server on port 3000');
